df6ae8540e04132d58eefb4fe4db9f1553f0d1d4
logeswaranV <logeswaran@vthink.co.in>
2026-07-16 00:10:17 +0530

    md files updates

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,12 @@
+public class ReverseArray {
+    
+    public static void main(string[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        
+        // Print the reversed array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ Arrays/patterns.md
@@ -0,0 +1,6 @@
+# Patterns in Arrays
+
+1. Two pointer
+    - Must the Array is in sort order
+    - Finding pair
+    - Best for avoid nested loops
