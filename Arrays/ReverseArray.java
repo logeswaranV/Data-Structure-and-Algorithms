bc6d993f521ea8fdb03971ca37bc686ba3513a94
logeswaranV <logeswaran@vthink.co.in>
2026-07-16 00:37:04 +0530

    two pointer

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,12 +1,29 @@
+import java.util.Arrays;
+
 public class ReverseArray {
-    
-    public static void main(string[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        reverseArray(arr);
-        
-        // Print the reversed array
-        for (int num : arr) {
-            System.out.print(num + " ");
+
+    public static int[] reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Original Array: ");
+        System.out.println(Arrays.toString(arr));
+
+        int[] reversedArr = reverseArray(arr);
+        System.out.println("Reversed Array: ");
+        System.out.println(Arrays.toString(reversedArr));
     }
 }
---
+++ Arrays/TwoSumProblem.java
@@ -0,0 +1,33 @@
+public class TwoSumProblem {
+
+    public static int[] twoSum(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else right--;
+        }
+
+        return new int[]{-1, -1};
+
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        int target = 6;
+
+        int[] result = twoSum(nums, target);
+
+        System.err.println( "result" + ": " + result[0] + " | " + result[1]);
+    }
+        
+        
+
+}
